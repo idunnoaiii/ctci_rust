415d027d054c134fdf3772a08bdc4731bf1bfe48
Thien.Nguyen <idunno.aiii@gmail.com>
2023-05-14 23:24:45 +0700

    commit problem 3 chapter 1

--- src/bin/c01p03.rs
+++ src/bin/c01p03.rs
@@ -1,17 +1,15 @@
-
 fn urlify<'a>(url: &'a str) -> String {
-
     let mut bag = Vec::<String>::new();
 
-    for (idx, chr ) in url.char_indices() {
-        if chr == ' ' && (idx < url.len() -1  && url.chars().nth(idx + 1).unwrap() != ' ') {
+    for (idx, chr) in url.char_indices() {
+        if chr == ' ' && (idx < url.len() - 1 && url.chars().nth(idx + 1).unwrap() != ' ') {
             bag.push("%20".to_string());
         } else {
             if chr != ' ' {
                 bag.push(chr.to_string());
             }
         }
-    } 
+    }
 
     return bag.join("").as_str().to_string();
 }
@@ -20,7 +18,6 @@ fn urlify<'a>(url: &'a str) -> String {
 mod tests {
     use crate::*;
 
-
     #[test]
     fn test_urlify() {
         assert_eq!(urlify("Mr John Smith    "), "Mr%20John%20Smith");
@@ -28,5 +25,5 @@ mod tests {
 }
 
 fn main() {
-    println!("{}",urlify("Mr John Smith    "));
+    println!("{}", urlify("Mr John Smith    "));
 }
